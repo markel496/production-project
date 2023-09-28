6398d3b52b07972303f548f43d8b966f1a8e71c6
Ivan <markelov.ivan1996@yandex.ru>
2023-09-28 17:48:17 +0300

    add GitHub Actions

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -21,15 +21,17 @@ export function buildPlugins({
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev)
-    }),
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false
     })
   ]
 
   if (isDev) {
     plugins.push(new ReactRefreshWebpackPlugin())
     plugins.push(new webpack.HotModuleReplacementPlugin())
+    plugins.push(
+      new BundleAnalyzerPlugin({
+        openAnalyzer: false
+      })
+    )
   }
 
   return plugins
