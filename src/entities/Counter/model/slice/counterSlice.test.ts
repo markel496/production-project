4f0255ab026927bb3d01089bd4395fda979be28d
Ivan <markelov.ivan1996@yandex.ru>
2025-03-17 18:28:25 +0300

    update main.yml, packages

--- src/entities/Counter/model/slice/counterSlice.test.ts
+++ src/entities/Counter/model/slice/counterSlice.test.ts
@@ -1,24 +1,26 @@
 import { CounterSchema } from '../types/counterSchema'
 
-import { counterActions, counterReducer } from './counterSlice'
+import { useCounterActions, counterReducer } from './counterSlice'
 
 describe('CounterSlice.test', () => {
+  const { increment, decrement } = useCounterActions()
+
   test('increment', () => {
     const state: CounterSchema = { value: 10 }
-    expect(counterReducer(state, counterActions.increment())).toEqual({
+    expect(counterReducer(state, increment())).toEqual({
       value: 11
     })
   })
 
   test('decrement', () => {
     const state: CounterSchema = { value: 10 }
-    expect(counterReducer(state, counterActions.decrement())).toEqual({
+    expect(counterReducer(state, decrement())).toEqual({
       value: 9
     })
   })
 
   test('should work with empty state', () => {
-    expect(counterReducer(undefined, counterActions.increment())).toEqual({
+    expect(counterReducer(undefined, increment())).toEqual({
       value: 1
     })
   })
